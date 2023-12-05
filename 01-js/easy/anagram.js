beecbed1ee3e02b3ee10d33c0a6496c425483d2f
Shubham Dalvi <shubham.dalvi008@gmail.com>
2023-12-05 10:37:15 +0530

    solved anagram problem

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,21 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if (str1.length !== str2.length) return false;
+  const isLetterPresent = str1
+    .toLowerCase()
+    .split("")
+    .map((letter) => str2.toLowerCase().includes(letter));
+  const res = isLetterPresent.every((i) => i === true);
+  return res;
 }
+
+// function isAnagram(str1, str2) {
+//   let a = str1.toLowerCase();
+//   let b = str2.toLowerCase();
+//   if (str1.length !== str2.length) return false;
+//   const res = a.split("").sort().join("") === b.split("").sort().join("");
+//   return res;
+// }
 
 module.exports = isAnagram;
